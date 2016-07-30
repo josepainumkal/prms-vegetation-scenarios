cd247c78020d8ef0266b3df4efb24e491c48aa63
ruiwu1990 <raywu1990@gmail.com>
2016-07-29 17:13:01 -0700

    add checkbox to line chart

--- app/static/js/vis/vis_stat.js
+++ app/static/js/vis/vis_stat.js
@@ -5,10 +5,11 @@ $(document).ready(function(){
   var scenarioID = currentURL[currentURL.length-1]; 
   var timeArr;
   var paramList;
+  var paramDataArrLen;
 
   // line chart part
   google.charts.load('current', {'packages':['corechart']});
-  google.charts.setOnLoadCallback(drawChart);
+  
   var lineChartData = [];
   var lineChartName = ['Time'];
 
@@ -23,24 +24,69 @@ $(document).ready(function(){
     }
 
     $('#confirmParamButtonID').on('click',function(){
-      $.get('/api/netCDF_stat_data/'+scenarioID+'/'+$('paramSelectBoxID').val(),function(paramData){
+      $.get('/api/netCDF_stat_data/'+scenarioID+'/'+$('#paramSelectBoxID').val(),function(paramData){
         var receivedParamJson = JSON.parse(paramData);
         var paramDataArr = receivedParamJson['param_data'];
-        lineChartName.append($('paramSelectBoxID').val());
-        lineChartData.append(lineChartName);
+        paramDataArrLen = paramDataArr.length;
+        // initialize
+        lineChartData = [];
 
+        lineChartName.push($('#paramSelectBoxID').val());
+        lineChartData.push(lineChartName);
+
+        for(var i=0; i<paramDataArrLen; i++)
+        {
+          lineChartData.push([parseFloat(timeArr[i]),parseFloat(paramDataArr[i])]);
+        }
+        // create checkbox for 
         for(var i=0; i<nameListLen; i++)
         {
-          lineChartData.append([timeArr[i],paramData[i]]);
+          if(paramList[i] != $('#paramSelectBoxID').val())
+          {
+            $('#checkBoxDivID').append("<label class='checkbox-inline'><input type='checkbox' id='"+paramList[i]+"' value='"+paramList[i]+"'>"+paramList[i]+"</label>"); 
+          }
         }
-
+        google.charts.setOnLoadCallback(drawChart);
       });
     });
     
   });
+  // $(document).on('change','[type=checkbox]',function(){
+  $(document).on('change','[type=checkbox]',function(){
+    var checkboxID = this.value;
+    if($("#"+checkboxID).prop("checked"))
+    {
+      $.get('/api/netCDF_stat_data/'+scenarioID+'/'+paramName,function(chosenParamData){
+        var chosenParamJSON = JSON.parse(chosenParamData);
+        var chosenParamDataArr = chosenParamJSON['param_data'];
+        lineChartName.push(checkboxID);
+        for(var i=0; i<paramDataArrLen; i++)
+        {
+          var tempArr = lineChartData[i+1];
+          tempArr.push(parseFloat(chosenParamDataArr[i]));
+          lineChartData[i+1] = tempArr;
+        }
+      });
+    }
+    // else remove it
+    else
+    {
+      var chosenParamIndex = lineChartName.indexOf(checkboxID);
+      // remove element based on the index
+      lineChartName.splice(chosenParamIndex,1);
+      for(var i=0; i<paramDataArrLen; i++)
+      {
+        var tempArr = lineChartData[i+1];
+        tempArr.splice(chosenParamIndex,1);
+        lineChartData[i+1] = tempArr;
+      }
+    }
+
+    google.charts.setOnLoadCallback(drawChart);
+  });
 
   function drawChart() {
-    var data = google.visualization.arrayToDataTable([lineChartData]);
+    var data = google.visualization.arrayToDataTable(lineChartData);
 
     var options = {
       title: 'Stats data',
@@ -48,7 +94,8 @@ $(document).ready(function(){
       legend: { position: 'bottom' }
     };
 
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    //var chart = new google.visualization.LineChart($('#lineChartDivID'));
+    var chart = new google.visualization.LineChart(document.getElementById(lineChartDivID));
 
     chart.draw(data, options);
   }
